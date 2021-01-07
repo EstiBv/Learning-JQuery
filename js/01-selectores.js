39b0b3d6de3871ecfbcae76e988a49ee609dc3d5
Estibaliz Barato <estibalizbarato@gmail.com>
2021-01-07 11:32:45 +0100

    tag selectors

--- js/01-selectores.js
+++ js/01-selectores.js
@@ -11,10 +11,36 @@ $(document).ready(function () {
 
   // Aplicando estilos y encandenando propiedades
   let paragrapahChangeColor = $("#jq-yellow")
-    .css("background", "orange")
-    .css("color", "white");
+    .css("background", "yellow")
+    .css("color", "orange");
 
-  paragrapahGreen.css("font-size", "30px").css("color", "green");
-
+  paragrapahGreen.css("padding", "30px").css("background", "green");
   console.log(paragrapahGreen);
+
+  // Selector de clase
+  let paragrahImportant = $(".js-importantP")
+    .css("color", "red")
+    .css("border", "5px dashed black");
+  console.log(paragrahImportant.eq(0));
+
+  // Selectores de etiqueta : clase definida en file.css
+  //removeClass() >> remove() || hasClass() >> contains()
+
+  $("p").click(function () {
+    $(this).addClass("increaseFont");
+    console.log("click dado");
+  });
+
+  // Optimizar carga accediendo una vez al DOM
+  let selectP = $("p");
+
+  selectP.click(function () {
+    let that = $(this);
+    if (!that.hasClass("numbersSelect")) {
+      that.addClass("numbersSelect");
+    } else {
+      that.removeClass("numbersSelect");
+    }
+    console.log(that);
+  });
 });
