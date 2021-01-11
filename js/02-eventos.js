7b535f264d19f3ec74c7c3b72fc65849dd9bfbdf
Estibaliz Barato <estibalizbarato@gmail.com>
2021-01-11 10:37:53 +0100

    events mouse and hover

--- js/02-eventos.js
+++ js/02-eventos.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
   console.log("hola");
   // MouseOver and MouseOut >> pseudoclass :hover
+  const box = $("#box");
+  box.mouseover(function () {
+    $(this).css("background", "red");
+  });
+  box.mouseout(function () {
+    $(this).css("background", "green");
+  });
 });
