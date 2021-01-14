e1f507209e87c11900a3af255540c17e03524f99
Estibaliz Barato <estibalizbarato@gmail.com>
2021-01-14 09:54:37 +0100

    method Get - Ayax

--- js/05-ayax.js
+++ js/05-ayax.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
-  //  Load() => peticion Ayax por Get e incrustar el resultado en elemento html
+  //  Metodo Load() => peticion Ayax por Get e incrustar el resultado en elemento html
+
   const data = $(".js-data");
   data.load("https://reqres.in/");
+
+  // Metodo Get() para peticion GET
+  $.get("https://reqres.in/api/users", { page: 2 }, function (response) {
+    console.log(response);
+    console.log(
+      response.data.forEach((each, index) => {
+        $(".js-dataUser").append(
+          "<p>" + index + " " + each.first_name + " " + each.last_name + "</p>"
+        );
+      })
+    );
+  });
+
+  // Metodo Post() para peticion POST
 });
